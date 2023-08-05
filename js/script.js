8cb8a426150258083462f54731c0de8461136480
Büşra Özata <busraozata1998@gmail.com>
2023-08-05 09:53:36 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -56,6 +56,44 @@ var swiper = new Swiper(".mySwiperSSSArea", {
   },
 });
 
+var swiperServiceSlide = new Swiper(".swiperServiceSlide", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: "#swiper-button-nextService",
+    prevEl: "#swiper-button-prevService",
+  },
+});
+
+var swiperGallery = new Swiper(".ImgGalleryMySwiper", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
+
 const header = document.querySelector(".header");
 const headerHeight = parseInt(window.getComputedStyle(header).height);
 
